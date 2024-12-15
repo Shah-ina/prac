c0d52a01b0064106055daea23a7151f84561c446
Shahina Khatun <shahinasardar07@gmail.com>
2024-12-15 21:38:13 +0530

    Add files via upload
    It contains basics of Cancer genomics codes

---
+++ Cancergenomics.R
@@ -0,0 +1,11 @@
+# Cancer Genomics
+
+# downloading packages
+install.packages("hflights")
+install.packages("askpass")
+install.packages("BiocManager")
+library(BiocManager)
+install.packages("Biostrings")
+BiocManager::install("Biostrings")
+install.packages("testthat")
+
